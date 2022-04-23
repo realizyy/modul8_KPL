858473887e76d15cebd7e543d49dbcfda0adb65d
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-23 12:51:22 +0800

    Menambahkan API Movies

---
+++ Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace modul8_1302204035.Properties;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public List<string> Stars { get; set; }
+    public string Description { get; set; }
+
+
+    public Movie(string title, string director, List<string> stars, string description)
+    {
+        Title = title;
+        Director = director;
+        Stars = stars;
+        Description = description;
+    }
+
+    //create static movie list
+    public static List<Movie> MovieList = new List<Movie>()
+    {
+        new Movie("The Shawshank Redemption", "Frank Darabont", new List<string>(){ "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency." ),
+        new Movie("The Godfather", "Francis Ford Coppola", new List<string>(){ "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton "}, "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."),
+        new Movie("The Dark Knight", "Christopher Nolan", new List<string>(){ "Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine"}, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.")
+    };
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Components.Web;
+using modul8_1302204035.Properties;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,8 +38,45 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
-app.Run();
+//Movies
 
+var movies = new List<Movie>();
+//create new movie
+movies.Add(new Movie("The Shawshank Redemption", "Frank Darabont", new List<string>(){ "Tim Robbins", "Morgan Freeman", "Bob Gunton" }, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."));
+movies.Add( new Movie("The Godfather", "Francis Ford Coppola", new List<string>(){"Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton "}, "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."));
+movies.Add(new Movie("The Dark Knight", "Christopher Nolan", new List<string>(){"Christian Bale", "Heath Ledger", "Aaron Eckhart"}, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."));
+
+
+//create new api get movies
+app.MapGet("/api/Movies", () =>
+{
+    return movies;
+});
+
+//create new api post movies
+app.MapPost("/api/Movies", () =>
+{
+    return movies;
+});
+
+
+//create new api get movies id
+app.MapGet("/api/Movies/{id}", () =>
+{
+    int id = 0;
+    return movies[id];
+});
+
+//create new api delete movies id
+app.MapDelete("/api/Movies/{id}", () =>
+{
+    int id = 1;
+    movies.RemoveAt(id);
+    return movies;
+});
+
+
+app.Run();
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
